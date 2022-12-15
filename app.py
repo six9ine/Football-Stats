03aadd53d5d8211a7a189f898a3ffa6c13626b45
six9ine <34369175+six9ine@users.noreply.github.com>
2022-12-15 11:22:33 +0100

    team_stats with light error handling

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import http.client
-from flask import Flask, render_template
+import json
+import urllib
+
+from flask import Flask, render_template, request
+from json2html import json2html
 
 app = Flask(__name__)
 
@@ -8,22 +12,49 @@ app = Flask(__name__)
 def welcome():
     return "Bienvenue sur Football Stats"
 
+
 @app.route('/teams')
 def teams():
-    # conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-    #
-    # headers = {
-    #     'x-rapidapi-host': "v3.football.api-sports.io",
-    #     'x-rapidapi-key': "7b5bd0444b772ea5848eebc8a730f77a"
-    # }
-    #
-    #
-    #
-    # conn.request("GET", "/teams/statistics", headers=headers)
-    #
-    # res = conn.getresponse()
-    # data = res.read()
-
-
-
     return render_template("teams.html")
+
+
+@app.route('/team_stats', methods=['GET', 'POST'])
+def team_stats():
+    try:
+        team_name = urllib.parse.quote_plus(request.args['team_name'])
+        league_name = urllib.parse.quote_plus(request.args['league_name'])
+        season = request.args['season'].strip()
+
+        conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+        headers = {
+            'x-rapidapi-host': "v3.football.api-sports.io",
+            'x-rapidapi-key': "45489aa3142e552f28903cd2fb5e92de"
+        }
+
+        conn.request("GET", "/teams?name=" + team_name, headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        team_id = json.loads(data)["response"][0]["team"]["id"]
+
+        conn.request("GET", "/leagues?name=" + league_name, headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        league_id = json.loads(data)["response"][0]["league"]["id"]
+
+        conn.request("GET",
+                     "/teams/statistics?league=" + str(league_id) + "&season=" + season + "&team=" + str(team_id),
+                     headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        stats = json.loads(data)["response"]
+
+        return json2html.convert(json=stats)
+    except:
+        return render_template("error.html")
